24aac45c8942ed9c43227d248ac9e747c8de5717
Ouriel Boiria <ouriel.boiria@gmail.com>
2019-01-03 10:04:51 +0100

    Reorganisation of advanced search react components

--- app/javascript/packs/public-bundle.js
+++ app/javascript/packs/public-bundle.js
@@ -1,13 +1,13 @@
 import ReactOnRails from 'react-on-rails';
 
-import ReferenceSearchContainer from '../bundles/AdvancedSearch/components/ReferenceSearchContainer';
-import ReferenceSearch from '../bundles/AdvancedSearch/components/ReferenceSearch';
-import SelectedReferenceSearch from '../bundles/AdvancedSearch/components/SelectedReferenceSearch';
-import ItemTypesReferenceSearch from '../bundles/AdvancedSearch/components/ItemTypesReferenceSearch';
-import DateTimeSearch from '../bundles/AdvancedSearch/components/DateTimeSearch';
+import ReferenceSearchContainer from '../bundles/AdvancedReferenceSearch/components/ReferenceSearchContainer';
+import ReferenceSearch from '../bundles/AdvancedReferenceSearch/components/ReferenceSearch';
+import SelectedReferenceSearch from '../bundles/AdvancedReferenceSearch/components/SelectedReferenceSearch';
+import ItemTypesReferenceSearch from '../bundles/AdvancedReferenceSearch/components/ItemTypesReferenceSearch';
+import DateTimeSearch from '../bundles/AdvancedDateTimeSearch/components/DateTimeSearch';
 import DateTimeInput from '../bundles/DateTimeInput/components/DateTimeInput';
-import ChoiceSetSearchContainer from '../bundles/AdvancedSearch/components/ChoiceSetSearchContainer';
-import ChoiceSetSearch from '../bundles/AdvancedSearch/components/ChoiceSetSearch';
+import ChoiceSetSearchContainer from '../bundles/AdvancedChoiceSetSearch/components/ChoiceSetSearchContainer';
+import ChoiceSetSearch from '../bundles/AdvancedChoiceSetSearch/components/ChoiceSetSearch';
 
 import ImageViewer from '../bundles/ImageViewer/components/ImageViewer';
 import GeoViewer from '../bundles/GeoViewer/components/GeoViewer';
