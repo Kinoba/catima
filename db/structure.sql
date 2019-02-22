3bf75d3df2d21304354fe3dc99850260a61f6cef
Ouriel Boiria <ouriel.boiria@gmail.com>
2019-02-22 16:15:07 +0100

    Fix rebase issues

--- db/structure.sql
+++ db/structure.sql
@@ -9,20 +9,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
---
 -- Name: bigdate_to_num(json); Type: FUNCTION; Schema: public; Owner: -
 --
 
@@ -36,17 +22,6 @@ CREATE FUNCTION public.bigdate_to_num(json) RETURNS numeric
             CASE WHEN $1->>'m' IS NULL THEN 0 ELSE ($1->>'m')::INTEGER * POWER(10, 2) END +
             CASE WHEN $1->>'s' IS NULL THEN 0 ELSE ($1->>'s')::INTEGER END
           )::NUMERIC;$_$;
-
-
---
--- Name: strip_tags(text); Type: FUNCTION; Schema: public; Owner: -
---
-
-CREATE FUNCTION public.strip_tags(text) RETURNS text
-    LANGUAGE sql
-    AS $_$
-          SELECT regexp_replace($1, '<[^>]*>', '', 'g')
-          $_$;
 
 
 --
@@ -80,10 +55,10 @@ CREATE TABLE public.advanced_search_configurations (
     title_translations jsonb,
     description jsonb,
     slug character varying,
+    search_type character varying DEFAULT 'default'::character varying,
     fields jsonb DEFAULT '{}'::jsonb,
     created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL,
-    search_type character varying DEFAULT 'default'::character varying NOT NULL
+    updated_at timestamp without time zone NOT NULL
 );
 
 
